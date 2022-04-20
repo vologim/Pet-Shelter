67cfc45b66ed20287d0bf8849d3a1a907f7edd2e
Misha <golovackii@gmail.com>
2022-04-20 23:01:21 +0300

    I make controller

--- src/main/java/com/mikhail/golovackii/pet_shelter/model/PetBreed.java
+++ src/main/java/com/mikhail/golovackii/pet_shelter/model/PetBreed.java
@@ -3,24 +3,34 @@ package com.mikhail.golovackii.pet_shelter.model;
 public enum PetBreed {
 
 //    Dogs
-    OUTBRED_DOG,
-    LABRADOR_RETRIEVER,
-    YORKSHIRE_TERRIER,
-    TAKSA,
-    PEKINGESE,
-    CHIHUAHUA,
-    ROTTWEILER,
-    CORGI,
-    BULLDOG,
-    HUSKY,
-    PUG,
+    OUTBRED_DOG("дворняжка"),
+    LABRADOR_RETRIEVER("лабрадор"),
+    YORKSHIRE_TERRIER("йоркшер"),
+    TAKSA("такса"),
+    PEKINGESE("пекинес"),
+    CHIHUAHUA("чихуахуа"),
+    ROTTWEILER("ротвейлер"),
+    CORGI("корги"),
+    BULLDOG(",ekmlju"),
+    HUSKY("хаски"),
+    PUG("мопс"),
 
 //    Cats
-    OUTBRED_CAT,
-    MAINE_COON,
-    SIBERIAN,
-    BRITISH,
-    SCOTTISH,
-    SPHINX,
-    SIAMESE
+    OUTBRED_CAT("метис"),
+    MAINE_COON("мейн-кун"),
+    SIBERIAN("себирская"),
+    BRITISH("британская"),
+    SCOTTISH("шотландская"),
+    SPHINX("сфинкс"),
+    SIAMESE("сиамская");
+
+    private final String value;
+
+    PetBreed(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
